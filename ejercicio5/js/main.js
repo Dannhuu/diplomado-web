67f398254d5f36a2fa3e1a53375a41cbde01930a
Alumno <alumno@iMac-de-Alumno.local>
2018-05-26 14:23:30 -0500

    AddjQuery validate plugin and PHPMailer plugin

--- ejercicio5/js/main.js
+++ ejercicio5/js/main.js
@@ -1,9 +1,57 @@
-/* 
-Ready function: http://api.jquery.com/ready/
-*
-*/
 
 $(function () {
-    // Code ...
-    console.log("Hola Mundo");
+  /* 
+    Selectorees JQuery, sintaxis:
+        $("[selector css] ")
+
+        Ejemplos:
+        $(".form") -> Esto va a selccionar a todos los elementos HTML
+        que tengan el atributo "class" con un valor "form"
+
+        $("#form") -> Esto va a seleccionar al elemnto HTML que tengan 
+        el atrubuto "id" con un valor "form"
+
+        $("p") -> Esto va a seleccionar al elemnto HTML tipo "p"
+
+        $("page.main page.section p a ") -> Esto va a seleccionar al elemnto HTML tipo "a"
+        que esten dentro de "p" que este dentro de un "page.section" que este dentro de
+        un "page.main"
+
+
+                Ejemplo de combinación de 2 objetos utilizando el metodo
+                (función) ".extend()" de JQuery
+
+
+         var alumno = {
+    nombre: "Daniel",
+    apellido: "Sánchez",
+    edad: 22,
+};
+var alumnoUpdate = {
+    nombre: "Daniel Alberto",
+    edad: 12,
+    ocupación: "Graphic Designer"
+};
+
+$.extend(alumno, alumnoUpdate)
+
+
+  */
+$.extend($.validator.messages, {
+    required: "Este campo es obligatorio",
+    email: "Por favor, escribe una dirección de correo válida.",
+    number: "Por favor escribe un numero entero valido",
+    rangelength: $.validator.format( "Por favor, escribe un valor entre {0} y {1} caracteres." ),
+});
+$("#form").validate({
+    rules:{
+        tel: {
+            rangelength: [6, 10],
+            number: true,
+        }
+    }
+});
+
+
+
 });
---
+++ ejercicio5/php/enviar.php
@@ -0,0 +1,68 @@
+<?php
+      $myName = "Daniel";
+      $mylastName = "Sánchez";
+      $myAge = 22;
+
+     $mydata = "Mi nombre completo es: "
+        . $myName
+        . " "
+        . $mylastName
+        . ", y tengo "
+        . $myAge
+        . "años.";
+
+    $name = $_POST ["name"];
+    $lastName = $_POST ["lasteName"];
+    $tel = $_POST ["tel"];
+    $email = $_POST ["email"];
+    $gender = $_POST ["gender"];
+    $read = $_POST ["read"];
+    $dance = $_POST ["dance"];
+    $run = $_POST ["run"];
+    $play = $_POST ["play"];
+    $naciolality = $_POST ["nacionality"];
+    $message = $_POST ["message"];
+    
+    $isReadSelected    = $read ?  "Si" : "No";
+    $isDanceSelected   = $dance ? "Si" : "No";
+    $isRunSelected     = $run ?   "Si" : "No";
+    $isPlaySelected    = $play ?  "Si" : "No";
+?>
+
+
+<!DOCTYPE html>
+<html lang  ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mis datos</title>
+</head>
+<body>
+    <div class="container">
+        <header class="page-header">
+            <h1>
+                Mis Datos
+            </h1>
+        </header>
+        <main class="page-main">
+            <section class="page-section">
+               <ul>
+                    <li>Nombre: <strong><?php echo $name; ?></strong> </li>
+                    <li>Apellidos:<strong><?php echo $lastName; ?></strong></li>
+                    <li>Teléfono:<strong><?php echo $tel; ?></strong></li>
+                    <li>Email:<strong><?php echo $email; ?></strong></li>
+                    <li>Sexo:<strong><?php echo $gender; ?></strong></li>
+                    <li>Leer:<strong><?php echo $isReadSelected; ?></strong></li>
+                    <li>Bailar:<strong><?php echo $isDanceSelected; ?></strong></li>
+                    <li>Correr<strong><?php echo $isRunSelected; ?></strong></li>
+                    <li>Jugar:<strong><?php echo $isPlaySelected ?></strong></li>
+                    <li>Nacionalidad:<strong><?php echo $naciolality; ?></strong></li>
+                    <li>Mensaje:<strong><?php echo $message; ?></strong></li>
+                 </ul>
+            </section>
+
+        </main>
+    </div>
+</body>
+</html>
