c417ad1109e619891fb50198d1bc8a93d858a66f
Alumno <alumno@iMac-de-Alumno.local>
2018-06-09 14:31:24 -0500

    Add axaples JQuery events

--- ejercicio-js/js/main.js
+++ ejercicio-js/js/main.js
@@ -52,6 +52,54 @@ $(function () {
         --  -> Decremento
 
 
+    3.- Operadores Relacionales: Siempre van a devolver un valor booleano 
+
+        >   Mayor que
+        <   Menor que
+        ==  Igual a
+        === Exáctamente igual a 
+        >=  Mayor o igual que
+        <=  Menos o igual que
+        !=  Diferente a
+        !== Exáctamente difetente a
+
+    4.- Operadores lógicos: Siempre van a devolver un valor booleando 
+                                
+        && AND
+
+           true && true    -> true 
+           true && false   -> false
+           false && true   -> false
+           false && false  -> false 
+
+        || OR
+
+           true || true    -> true
+           true || false   -> true
+           false || true   -> true
+           false || false  -> false
+
+
+    5.- Estructura de control "if else"
+           Sintaxis;
+                
+                    fi (condicional){
+                        La condicional debe de ser un valor booleano
+                        Si la condicional igual a "true" entonces se ejecuta el codigo
+                        que este dentro de estas llaves.
+                    }else{
+                        Si la condiconal es igual a "false" entonces se ejecuta el codigo
+                        que esta dentro de las llaves
+                    }
+
+
+    6.- iterador "for"
+        Sintaxis:
+                for (var i = 0; 1 < 10; i++){
+                    Código que se ejecuta en cada iteración
+                }
+
+
     */
 
     //STRING 
@@ -182,21 +230,21 @@ $(function () {
     var areas = $('#areas');
     //areas.append(triangulo1, triangulo2, triangulo3, triangulo4, triangulo5);
 
-    var ul = document.createElement('ul');
-    var li1 = document.createElement('li');
-    var li2 = document.createElement('li');
-    var li3 = document.createElement('li');
-    var li4 = document.createElement('li');
-    var li5 = document.createElement('li');
+    // var ul = document.createElement('ul');
+    // var li1 = document.createElement('li');
+    // var li2 = document.createElement('li');
+    // var li3 = document.createElement('li');
+    // var li4 = document.createElement('li');
+    // var li5 = document.createElement('li');
 
 
-    $(li1).append(triangulo1);
-    $(li2).append(triangulo2);
-    $(li3).append(triangulo3);
-    $(li4).append(triangulo4);
-    $(li5).append(triangulo5);
-    $(ul).append(li1, li2, li3, li4, li5);
-    areas.append(ul);
+    // $(li1).append(triangulo1);
+    // $(li2).append(triangulo2);
+    // $(li3).append(triangulo3);
+    // $(li4).append(triangulo4);
+    // $(li5).append(triangulo5);
+    // $(ul).append(li1, li2, li3, li4, li5);
+    // areas.append(ul);
    
     
     //OBJECT
@@ -227,17 +275,166 @@ $(function () {
     }
 
 
-console.log(persona['name']);
-console.log(persona['lastName']);
-console.log(persona['pets'][0]['name']);
-console.log(persona['pets'][1]['name']);
-console.log('---------------')
-console.log(persona.name);
-console.log(persona.lastName);
-console.log(persona.pets[0].name);
-console.log(persona.pets[1].name);
+//console.log(persona['name']);
+//console.log(persona['lastName']);
+//console.log(persona['pets'][0]['name']);
+//console.log(persona['pets'][1]['name']);
+//console.log('---------------')
+//console.log(persona.name);
+//console.log(persona.lastName);
+//console.log(persona.pets[0].name);
+//console.log(persona.pets[1].name);
 
-persona.printFullName();
+//persona.printFullName();
+
+
+
+
+var numero5 = 10;
+var numero6 = 20;
+var numero7 = 6;
+var numero8 = 4;
+var texto8 = '4';
+var resultado = numero5 < numero6;
+var resultado2 = numero6 != (numero5 + numero7 + numero8);
+    //console.log(resultado);
+    //console.log(resultado2);
+var resultado3 = numero8 != texto8;
+var resultado4 = numero8 !== texto8;
+    //console.log('Diferenta a:', resultado3)
+    //console.log('Exáctamente diferente a:', resultado4)
+
+
+var isMexican= true;
+var isHappy = false;
+
+var isAcceptinFrance = isMexican && isHappy;
+var isAcceptInEU = isMexican || isHappy;
+
+    //console.log('Es aceptado en Francia:', isAcceptinFrance);
+    //console.log('Es aceptado en EU', isAcceptInEU)
+//if (isMexican && isHappy) {
+//console.log('Es aceptado en Francia');
+//} else {
+//console.log ('Es aceptado en Francia');
+//}
+
+
+
+// var values = 
+// {base:100,  height:200};
+// {base:105, height: 350};
+// {base:3876, height: 1984};
+// {base:245.34 , height: 54.12};
+// {base:1254.23, height: 7463.45};
+
+
+
+/*
+ Eventos:
+    - Click
+    - mouseenter
+    - mauseleave
+    - scroll
+
+Metodo ".on()" de JQuery: Permite esuchar eventos que se desencadenan
+                          en un evento HTML en particular
+    
+     Sintaxis; Recibe 2 parametros:
+
+        1.- El evento que queremos escuchar.
+        2.- La función que se va a ejcutar
+        cuando suceda el evento
+
+        O recibe un parámetro:
+
+        1.- Un objeto con pares:
+
+
+        elementoHTML.on(evento, fn)
+        elementoHTML.on(click, function() {...code})
+
+Metodo ".css()" de JQuery: Permite modicar estilos css
+
+     Sintaxis: Recibe 2 parametros
+
+        1.- La propiedad css que se quiere modificar
+        2.- El nuevo valor de dicha propiedad
+
+         O recibe un parámetro:
+
+        1.- Un objeto con pares:
+
+
+        elementoHTML.css(propiedad, valor);
+        elementoHTML.css('color', '#fff')
+
+Metodo "attr()" de JQuery:
+
+        1.- Si se pasa un solo parametro, este parametro debe de ser
+        el nombre de un atributo del elemento y entonces devolverá
+        el valor de éste atributo 
+
+        2.- Si se pasan 2 páramatros, el primero debe de ser el nombre del 
+        atributo que quwremos modificar y el segundo el nuevo valor de este
+        atributo
+
+
+        sintaxis;
+            elementoHTML.attr('src') -> Esto devolvera el valor del
+            estributo "src"
+
+            alementoHTML.attr('scr', 'image/001.jpg)
+
+
+*/
+
+var myButton = $('#myButton');
+var myWindow = $(window);
+// myButton.on('click', function(){
+
+//     console.log('Hice click sobre mi boton');
+// });
+
+// var myButton = $('#myButton');
+// myButton.on('mouseenter', function(){
+
+//     console.log('Estoy sobre el boton');
+// });
+
+// var myButton = $('#myButton');
+// myButton.on('mouseleave', function(){
+
+//     console.log('Estoy fuera del boton');
+// });
+
+var macImage = $('#macImage');
+//console.log(macImage);
+// var macImageScr = macImage.attr('src');
+// console.log(macImageScr);
+
+myButton.on({
+    click: function() {
+        macImage.attr('src', 'images/050.jpg');
+    },
+    mouseenter: function() {
+        myButton.css('background-color', '#f00');
+    },
+    mouseleave: function() {
+        myButton.css('background-color', '#555');
+    },
+});
+
+myWindow.on('scroll', function(){
+    var positionScroll = $(this).scrollTop();
+    var newSrc = 'images/0' + positionScroll + '.jpg';
+    if (positionScroll <= 74)  {
+        macImage.attr('src', newSrc)
+    }
+ 
+});
+
+
 
 
 });
